9c9dd128cd12697f2ac76502a33fb899b6657050
Felix Ocker <felix.ocker@googlemail.com>
2022-12-01 09:36:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# aoc2022
+[Advent of Code 2022](https://adventofcode.com/)
+
+## get data using aocd-data
+* [docs on pypi](https://pypi.org/project/advent-of-code-data/)
+* run `aocd DD YYYY > input.txt`
+
+
---
+++ template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""AOC 2022 day X pt Y"""
+
+
+from aocd.models import Puzzle
+
+
+def main(data):
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    with open("input_pt_1.txt") as f:
+        result = main(f.read())
+    # assert result == ??, f"result not as expected: {result}"
+    # puzzle = Puzzle(year=2022, day=01)
+    # print(main(puzzle.input_data))
