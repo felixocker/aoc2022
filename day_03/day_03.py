96ab4c82472be12e3edc43002e76cf08e9a87d53
Felix Ocker <felix.ocker@googlemail.com>
2022-12-03 12:37:40 +0100

    day 03: helper function to abort search at first common item found

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -7,12 +7,19 @@ import string
 from aocd.models import Puzzle
 
 
+def first_common_elem(stra: str, strb: str) -> str:
+    for a in set(stra):
+        for b in set(strb):
+            if a == b:
+                return a
+
+
 def part_a(data):
     priorities = {v: c+1 for c, v in enumerate(string.ascii_letters)}
     rucksacks = [(i[:len(i)//2], i[len(i)//2:]) for i in data.split("\n")]
     prio_sum = 0
     for r in rucksacks:
-        prio_sum += priorities[list(set(r[0]) & set(r[1]))[0]]
+        prio_sum += priorities[first_common_elem(r[0], r[1])]
     return prio_sum
 
 
@@ -20,10 +27,9 @@ def part_b(data):
     priorities = {v: c + 1 for c, v in enumerate(string.ascii_letters)}
     rucksacks = [i for i in data.split("\n")]
     prio_sum = 0
-    while rucksacks:
-        e1, e2, e3 = rucksacks[:3]
-        rucksacks = rucksacks[3:]
-        prio_sum += priorities[list(set(e1) & set(e2) & set(e3))[0]]
+    for i in range(len(rucksacks) // 3):
+        e1, e2, e3 = rucksacks[i*3:i*3+3]
+        prio_sum += priorities[first_common_elem("".join(set(e1) & set(e2)), e3)]
     return prio_sum
 
 
